5629f2570fc9a3a669dffb2064e46efa52670cac
Tobias Kalcher <tkalcher@schueler.hakzell.at>
2020-04-15 15:32:09 +0200

    stunde15042020

--- geschichte-app/src/app/home/home.page.ts
+++ geschichte-app/src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {v4 as uuidv4} from 'uuid';
+import { ReturnStatement } from '@angular/compiler';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,32 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+
+topics= [
+  {id: uuidv4(), title:'Augsburger Reichs- und Religionsfriede', content: 'Dieser Friede wurde 1555...'},
+  {id: uuidv4(), title:'Kalter Krieg', content:'Auseinandersetzung zwischen USA und UdSSR'},
+  {id: uuidv4(), title:'Kalter Krieg', content:'Auseinandersetzung nach 2. Weltkrieg'}
+];
+
+
   constructor() {}
 
-}
+  ionViewWillEnter(){
+    let id= uuidv4();
+    console.log(this.topics);
+  }
+
+
+  showTopic(id){
+    const pos = this.topics.findIndex((t)=>{
+    return t.id==id;});
+
+    if (pos > -1){
+      const title= this.topics[pos].title;
+      console.log ('Titel: ' + title);
+    }
+
+    
+  
+
+  }
